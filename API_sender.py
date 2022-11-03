ec9ad0e6d2f2aa2ea2316f50796014a58334a112
Mickael Tessier <mickael_tessier@hotmail.fr>
2022-11-03 15:55:14 +0100

    API sender dvpt

--- API_sender.py
+++ API_sender.py
@@ -1,11 +1,23 @@
 import pandas as pd
+from requests import put
+import threading
+import time
 
-dic = {"time" : "2018-06-08T00:15:00.000Z", "DO" : 2.2, "NH4" : 3.1, "N2O" : 0.1, "NO3" : 4.2}
+#dic = {"time" : "2018-06-08T00:15:00.000Z", "DO" : 2.2, "NH4" : 3.1, "N2O" : 0.1, "NO3" : 4.2}
 
-df = pd.read_csv(r'C:\Users\micka\Desktop\Real-Time.csv')
+df = pd.read_csv(r'C:\Users\micka\Desktop\API.csv')
+#df = df.append(dic, ignore_index=True)
 
+#def hello_world():
+#    threading.Timer(10.0, hello_world).start() # called every minute
+#    print("Hello, World!")
+#hello_world()
 
-print(df)
+dic_df = df.to_dict(orient='records')
 
-df = df.append(dic, ignore_index=True)
-print(df)
+for t in dic_df:
+    time.sleep(5)
+
+    t = str(t).replace("'",'"')
+    print(t)
+    put('https://n2orisk-dev.eba-eknsupnb.us-east-1.elasticbeanstalk.com/api/v1/asset/test', data={'data': t})
